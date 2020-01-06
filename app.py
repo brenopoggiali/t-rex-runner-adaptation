c0be77281ab141cb3c98b16a683726f4a0e90742
Breno Poggiali <brenopoggiali@gmail.com>
2020-01-06 00:27:21 -0200

    Update Heroku files

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
